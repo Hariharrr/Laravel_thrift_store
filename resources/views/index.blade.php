d4642fe1eefec6ec882d7c792d4998e14f54119d
Hariharrr <henryhorrid308@gmail.com>
2024-09-25 20:58:29 +0545

    navbarimport

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello Thrift Store Project</h1>
-    <br>
-    <h2>we will starting soon......</h2>
-    <br>
-    <h3>stay in touch</h3>
-    <h1>okay </h1>
-    import { ThriftContext } from "../../context/Context"
+import { ThriftContext } from "../../context/Context"
 import styles from "./navbar.module.css"
 import {CiSearch,CiShoppingCart,CiBellOn} from "react-icons/ci"
 import {useContext} from "react"
@@ -72,6 +58,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-
-</body>
-</html>
